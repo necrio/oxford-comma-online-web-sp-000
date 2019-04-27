6a6bddc387702d6094b89c49024554ac2612aca6
Keith Isham <kisham8899@gmail.com>
2019-04-27 14:11:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
+when 1
+  "#{array[0]}"
+end
 
 
+<<<<<<< HEAD
   case array.length
 
   	when 1
@@ -11,6 +15,8 @@ def oxford_comma(array)
   else
     array[0...-1].join(", ") << ", and #{array[-1]}"
   end
+=======
+>>>>>>> 99f171582b58a8aaeaea65a1dab180a2421009b2
 
 
 
@@ -26,4 +32,7 @@ def oxford_comma(array)
 
 
 end
+<<<<<<< HEAD
 0
+=======
+>>>>>>> 99f171582b58a8aaeaea65a1dab180a2421009b2
